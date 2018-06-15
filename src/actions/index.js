342d0a91eac4d96d8a9f1653c955b94e64fd5512
Victor Ponomarenko <victor.ponomarenko.o@gmail.com>
2018-06-16 02:06:28 +0300

    Update README.md

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '4fabbd4a7da05522a1d5ff4392cfb32d';
+const API_KEY = 'API_KEY';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
